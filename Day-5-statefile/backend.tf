7e38dc570e7b15f5cbafd658725f28b6b583b5fc
Nayab Rasool <rasool330@gmail.com>
2026-07-26 17:51:23 +0530

    Day4,5 total

--- Day-5-statefile/backend.tf
+++ Day-5-statefile/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "dev-test-prod-nayab"  #give bucket name which you have created in AWS S3 to upload the tfstate file.
-    key    = "terraform.tfstate"
+    key    = "Day-5/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true  #if terraform version is >1.10
     #dynamodb_table = "terraform-state-locking" #if terraform version is <1.10. give the name of the dynamodb table which you have created in AWS DynamoDB to lock the state file.
   }
 }
-
