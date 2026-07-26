c9f2459a500f608994488b745d191cb56ad46be3
Nayab Rasool <rasool330@gmail.com>
2026-07-26 16:55:17 +0530

    lockfile

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "dev-test-prod-nayab"  #give bucket name which you have created in AWS S3 to upload the tfstate file.
     key    = "terraform.tfstate"
     region = "us-east-1"
+    use_lockfile = true
   }
 }
 
